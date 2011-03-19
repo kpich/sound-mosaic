ab9789a39e6d2e847a46250a2a8b1ec4936c839e
Karl Pichotta <karlpichotta@gmail.com>
2011-03-19 03:18:26 -0500

    process source/dest separately now.

--- make-sound-mosaic.py
+++ make-sound-mosaic.py
@@ -1,16 +1,39 @@
 import marsyas
 import optparse
+import os
 import subprocess
-import sys
 
-def extract_feature_windows(files):
-    for f in files:
-        subprocess.Popen(['bextract', f,
-                          '-w', 'features-' + f + '.arff',
-                          '-p', '/dev/null'])
+OUTDIR = 'output'
+SAMPLE_RATE = 44100
 
-def parse_command_line_opts()
-    '''returns the OptionParser.
+def optionally_add_output_dir():
+    try:
+        os.mkdir(OUTDIR)
+    except:
+        pass
+
+def extract_source_features(sourcename, outputid, windowsize):
+    call_bextract(sourcename, 'output/' + outputid + '-src.arff', windowsize)
+
+def extract_dest_features(destname, outputid, windowsize):
+    call_bextract(destname, 'output/' + outputid + '-dest.arff', windowsize)
+
+def call_bextract(wavfile, outfile, windowSizeInMS):
+    windowSampleSize = (SAMPLE_RATE / 1000) * windowSizeInMS
+    subprocess.Popen(['bextract', wavfile,
+                      '-w', outfile,
+                      '-ws', str(windowSampleSize),
+                      '-hp', str(windowSampleSize),
+                      '-p', '/dev/null']).communicate()
+
+def match_windows(outputid):
+    pass
+
+def create_output_wavfile(outputid):
+    pass
+
+def parse_command_line_opts():
+    '''returns the OptionParser's parsed options.
         We're stuck with python26 so can't use an argparser!
     '''
     parser = optparse.OptionParser(description='Create a new sound mosaic.')
@@ -22,12 +45,17 @@ def parse_command_line_opts()
                         help='the window size, in milliseconds')
     parser.add_option('--output', type='string',
                         help='an identifier for output files')
-    return parser
+    return parser.parse_args()[0]
 
 if __name__ == '__main__':
-    parse_command_line_opts()
-    extract_source_features()
-    extract_dest_features()
-    match_windows()
-    create_output_wavfile()
+    options = parse_command_line_opts()
+    optionally_add_output_dir()
+    extract_source_features(options.source,
+                            options.output,
+                            options.windowsize)
+    extract_dest_features(options.source,
+                          options.output,
+                          options.windowsize)
+    match_windows(options.output)
+    create_output_wavfile(options.output)
 
